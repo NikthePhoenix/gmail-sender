3055f15600039c3b6acb3c7c236f9ca3ef297290
NikthePhoenix <127707188+NikthePhoenix@users.noreply.github.com>
2024-02-16 14:41:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    rabbitMQ: {
+      url: "amqp://localhost",
+      exchangeName: "mailExchange",
+    },
+  
+    mail: {
+      user: 'stormerselite@gmail.com',
+      pass: 'zogv iaah xhdq ptpo', 
+      from: 'BookMyEvent <youremail@gmail.com>'
+    },
+  
+  
+  };
